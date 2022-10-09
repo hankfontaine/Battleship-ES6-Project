8785b45af8011e2739e9299fce7e4d5327c00f5a
HankFontaine <henrymcgill@gmail.com>
2022-10-09 18:35:35 +0700

    set up basic layout

--- script.js
+++ script.js
@@ -351,3 +351,39 @@
   ///////////////// END OF BUSINESS LOGIC MODULES ///////////////////////
   ///////////////////////////////////////////////////////////////////////
 })();
+
+const mainContainer = document.querySelector('.main');
+
+const header = document.querySelector('.header');
+const gameTitle = document.createElement('div');
+gameTitle.classList.add('header-text');
+gameTitle.append('BATTLESHIP');
+header.appendChild(gameTitle);
+
+const body = document.querySelector('.body');
+
+const mainGameplayWindow = document.createElement('div');
+mainGameplayWindow.classList.add('main-gameplay-window');
+body.appendChild(mainGameplayWindow);
+
+const playerOffenseBoardContainer = document.createElement('div');
+playerOffenseBoardContainer.classList.add('player-offense-board-container');
+mainGameplayWindow.appendChild(playerOffenseBoardContainer);
+
+const playerOffensiveGridArea = document.createElement('div');
+playerOffensiveGridArea.classList.add('grid-area');
+playerOffenseBoardContainer.appendChild(playerOffensiveGridArea);
+
+const playerDefenseBoardContainer = document.createElement('div');
+playerDefenseBoardContainer.classList.add('player-defense-board-container');
+mainGameplayWindow.appendChild(playerDefenseBoardContainer);
+
+const playerDefensiveGridArea = document.createElement('div');
+playerDefensiveGridArea.classList.add('grid-area');
+playerDefenseBoardContainer.appendChild(playerDefensiveGridArea);
+
+const footer = document.querySelector('.footer');
+const footerText = document.createElement('div');
+footerText.classList.add('footer-text');
+footerText.append('c Hank');
+footer.appendChild(footerText);
