693dd05b393104a5a5eacddfc87625e8502fb443
HankFontaine <henrymcgill@gmail.com>
2022-10-11 13:38:44 +0700

    set up start of dom manipulation

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+///////////////////////////////////////////////////////////////////////
+///////////////// BEGIN DOM MANIPULATION LOGIC MODULES ///////////////////////
+///////////////////////////////////////////////////////////////////////
+
+const mainContainer = document.querySelector('.main');
+
+const header = document.querySelector('.header');
+const gameTitle = document.createElement('div');
+gameTitle.classList.add('header-text');
+gameTitle.append('BATTLESHIP');
+header.appendChild(gameTitle);
+
+const body = document.querySelector('.body');
+
+const mainGameplayWindow = document.createElement('div');
+mainGameplayWindow.classList.add('main-gameplay-window');
+body.appendChild(mainGameplayWindow);
+
+const playerOffenseBoardContainer = document.createElement('div');
+playerOffenseBoardContainer.classList.add('player-offense-board-container');
+mainGameplayWindow.appendChild(playerOffenseBoardContainer);
+
+const playerOffensiveGridArea = document.createElement('div');
+playerOffensiveGridArea.classList.add('grid-area');
+playerOffenseBoardContainer.appendChild(playerOffensiveGridArea);
+
+const playerDefenseBoardContainer = document.createElement('div');
+playerDefenseBoardContainer.classList.add('player-defense-board-container');
+mainGameplayWindow.appendChild(playerDefenseBoardContainer);
+
+const playerDefensiveGridArea = document.createElement('div');
+playerDefensiveGridArea.classList.add('grid-area');
+playerDefenseBoardContainer.appendChild(playerDefensiveGridArea);
+
+const footer = document.querySelector('.footer');
+const footerText = document.createElement('div');
+footerText.classList.add('footer-text');
+footerText.append('c Hank');
+footer.appendChild(footerText);
+
+///////////////////////////////////////////////////////////////////////
+///////////////// END DOM MANIPULATION LOGIC MODULES ///////////////////////
+///////////////////////////////////////////////////////////////////////
+
 (function runModulesOfGame() {
   ///////////////////////////////////////////////////////////////////////
   /////////////// BEGIN CODE OF MODULES PROJECT /////////////////////////
@@ -176,6 +220,48 @@
     };
   }
 
+  ///////////////////////////////////////////////////////////////////////
+  ///////////////// BEGIN DOM MANIPULATION LOGIC MODULES ///////////////////////
+  ///////////////////////////////////////////////////////////////////////
+
+  const gridArray = [];
+  const generateGridSquares = (board, grid, boardType) => {
+    board.forEach((square) => {
+      const newSquare = document.createElement('div');
+      newSquare.classList.add('square');
+      newSquare.id =
+        boardType.toString() +
+        +square.coordsOfSquare[0].toString() +
+        square.coordsOfSquare[1].toString();
+      grid.appendChild(newSquare);
+      gridArray.push(newSquare);
+    });
+  };
+
+  // generateGridSquares(
+  //   playerOne.defensiveBoard.squares,
+  //   playerDefensiveGridArea,
+  //   'defensive',
+  // );
+
+  // generateGridSquares(
+  //   playerOne.offensiveBoard.squares,
+  //   playerOffensiveGridArea,
+  //   'offensive',
+  // );
+
+  function findCoorespondingGridSquares(coords, context) {
+    // find matching squares to coords
+    // update classlist to new class
+    // can be used both for ship placement and attacks/defense
+  }
+
+  findCoorespondingGridSquares();
+
+  ///////////////////////////////////////////////////////////////////////
+  ///////////////// END DOM MANIPULATION LOGIC MODULES ////./////////////
+  ///////////////////////////////////////////////////////////////////////
+
   class Player {
     constructor(name) {
       this.name = name;
@@ -345,63 +431,6 @@
     ///////////////////////////////////////////////////////////////////////
     ///////////////// END OF BUSINESS LOGIC MODULES ///////////////////////
     ///////////////////////////////////////////////////////////////////////
-
-    const mainContainer = document.querySelector('.main');
-
-    const header = document.querySelector('.header');
-    const gameTitle = document.createElement('div');
-    gameTitle.classList.add('header-text');
-    gameTitle.append('BATTLESHIP');
-    header.appendChild(gameTitle);
-
-    const body = document.querySelector('.body');
-
-    const mainGameplayWindow = document.createElement('div');
-    mainGameplayWindow.classList.add('main-gameplay-window');
-    body.appendChild(mainGameplayWindow);
-
-    const playerOffenseBoardContainer = document.createElement('div');
-    playerOffenseBoardContainer.classList.add('player-offense-board-container');
-    mainGameplayWindow.appendChild(playerOffenseBoardContainer);
-
-    const playerOffensiveGridArea = document.createElement('div');
-    playerOffensiveGridArea.classList.add('grid-area');
-    playerOffenseBoardContainer.appendChild(playerOffensiveGridArea);
-
-    const playerDefenseBoardContainer = document.createElement('div');
-    playerDefenseBoardContainer.classList.add('player-defense-board-container');
-    mainGameplayWindow.appendChild(playerDefenseBoardContainer);
-
-    const playerDefensiveGridArea = document.createElement('div');
-    playerDefensiveGridArea.classList.add('grid-area');
-    playerDefenseBoardContainer.appendChild(playerDefensiveGridArea);
-
-    const footer = document.querySelector('.footer');
-    const footerText = document.createElement('div');
-    footerText.classList.add('footer-text');
-    footerText.append('c Hank');
-    footer.appendChild(footerText);
-
-    const generateGridSquares = (board, grid) => {
-      board.forEach((square) => {
-        const newSquare = document.createElement('div');
-        newSquare.classList.add('square');
-        newSquare.id =
-          +square.coordsOfSquare[0].toString() +
-          square.coordsOfSquare[1].toString();
-        grid.appendChild(newSquare);
-      });
-    };
-
-    generateGridSquares(
-      playerOne.defensiveBoard.squares,
-      playerDefensiveGridArea,
-    );
-
-    generateGridSquares(
-      playerOne.offensiveBoard.squares,
-      playerOffensiveGridArea,
-    );
   };
 
   ///////////////////////////////////////////////////////////////////////
